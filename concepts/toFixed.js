c0ce66db46f6a90d22e7d724201f96b89334dd28
Zibounne <tarik.dahhane.pro@gmail.com>
2025-04-05 14:00:35 +0200

    Adding: toFixed.js concept

---
+++ concepts/toFixed.js
@@ -0,0 +1,10 @@
+/*
+The toFixed() method converts a number to a string.
+
+The toFixed() method rounds the string to a specified number of decimals.
+*/
+
+let num = 5.56789;
+let result = num.toFixed(2);
+
+console.log(result);
