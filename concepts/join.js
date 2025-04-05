8b5a2c442bb5bf403bb241f8a3c74cf9f4fc04c0
Zibounne <tarik.dahhane.pro@gmail.com>
2025-04-05 13:46:55 +0200

    Adding : join.js concept

---
+++ concepts/join.js
@@ -0,0 +1,17 @@
+/*
+Syntax:
+array.join(separator)
+
+Parameters:
+separator	Optional.
+The separator to be used.
+Default is a comma.
+
+Return Value:
+A string.	The array values, separated by the specified separator.
+*/
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+let text = fruits.join(" ");
+
+console.log(text);
